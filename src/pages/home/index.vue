7d02d44ff9c4d07d70d21b353e8c3eca6d6813be
Alan-Xia <237490540@qq.com>
2021-06-01 18:00:17 +0800

    新增uni-app项目

---
+++ src/pages/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    首页
+    <uni-badge text="1"></uni-badge>
+  </div>
+</template>
+
+<script>
+import {uniBadge} from '@dcloudio/uni-ui';
+  export default {
+    components: {uniBadge}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
